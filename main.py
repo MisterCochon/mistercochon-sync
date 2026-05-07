c41bbe25fb4b2a6ec5329866be4e9474090fc703
MisterCochon <jfbuc@french-delicatessen.co.th>
2026-05-07 17:09:11 +0700

    Update main.py

--- main.py
+++ main.py
@@ -36,3 +36,22 @@ async def get_ecwid_products():
         )
 
     return response.json()
+import xmlrpc.client
+import os
+
+@app.get("/odoo/test")
+async def test_odoo():
+
+    url = os.getenv("ODOO_URL")
+    db = os.getenv("ODOO_DB")
+    username = os.getenv("ODOO_LOGIN")
+    password = os.getenv("ODOO_PASSWORD")
+
+    common = xmlrpc.client.ServerProxy(f"{url}/xmlrpc/2/common")
+
+    uid = common.authenticate(db, username, password, {})
+
+    if uid:
+        return {"status": "connected", "uid": uid}
+
+    return {"status": "failed"}
